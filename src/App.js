a0bebcdbb47e844dc5ce29b0ca5b0f1f9e95256c
Won-dy <junhfireace@gmail.com>
2022-01-04 14:28:26 +0900

    이벤트 준비, render 함수

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
     // 컴포넌트를 초기화 해주고 싶은 코드
     // 상위 컴포넌트 App의 state 값을 하위 컴포넌트 Subject, TOC, Content의 props 값으로 전달 가능
     this.state = {
+      mode:"read",
       subject:{title:'WEB', sub:'world wide web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -18,7 +20,19 @@ class App extends Component {
       ]
     }
   }
-  render() {
+
+  // React에서는 props나 state 값이 바뀌면 해당되는 컴포넌트의 render함수가 호출되도록 약속
+  // props나 state 값이 바뀌면 화면이 다시 그려짐
+  render() {  
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      _title = this.state.contents[2].title;
+      _desc = this.state.contents[2].desc;
+    }
     return (
       <div className="App">
         <Subject 
@@ -27,7 +41,7 @@ class App extends Component {
         </Subject>
         <Subject title="React" sub="For UI"></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
         <Content title="CSS" desc="CSS is Cascading Style Sheets."></Content>
       </div>
     );
