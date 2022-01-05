972790c8bf0d69ddf910c26a6488cea75a3cde38
Won-dy <junhfireace@gmail.com>
2022-01-05 12:08:32 +0900

    Update 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TOC from './components/TOC'
 import ReadContent from './components/ReadContent'
 import CreateContent from './components/CreateContent'
+import UpdateContent from './components/UpdateContent'
 import Subject from './components/Subject'
 import Control from './components/Control'
 import './App.css';
@@ -24,11 +25,19 @@ class App extends Component {
       ]
     }
   }
-
-  // React에서는 props나 state 값이 바뀌면 해당되는 컴포넌트의 render함수가 호출되도록 약속
-  // props나 state 값이 바뀌면 화면이 다시 그려짐
-  render() {  
-    console.log('App render');
+  getReadContent(){
+    // 클릭한 목록 정보 얻어오기
+    var i = 0;
+    while(i < this.state.contents.length){
+      var data = this.state.contents[i];
+      if(data.id === this.state.selected_content_id) {
+        return data;
+        break;
+      }
+      i = i + 1;
+    }
+  }
+  getContent(){
     var _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
@@ -36,17 +45,8 @@ class App extends Component {
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } 
     else if(this.state.mode === 'read'){
-      var i = 0;
-      while(i < this.state.contents.length){
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
     }
     else if(this.state.mode ==='create'){
       _article = <CreateContent onSubmit={function(_title, _desc){
@@ -61,24 +61,54 @@ class App extends Component {
         // #1-1. push() 쓰고 싶으면 Array.from 사용해서 복제 후 사용 / 배열일 때만 가능
         // Object.assign -> 객체를 바꾸고 싶을 때 사용
         // Array.frome -> 내용은 같지만 === 결과는 false이므로 shouldComponentUpdate 사용 가능
-        // var newContents = Array.from(this.state.contents);
-        // newContents.push({id:this.max_content_id, title:_title, desc:_desc});
-        // this.setState({
-        //   contents:newContents
-        // });
+        var newContents = Array.from(this.state.contents);
+        newContents.push({id:this.max_content_id, title:_title, desc:_desc});
+        this.setState({
+          contents:newContents,
+          mode:'read',
+          selected_content_id:this.max_content_id
+        });
 
         // #2. concat(): 원본 유지, 복제본 리턴
         // state 값 수정시 원본 수정X, 수정 복제본을 setState해라
         // shouldComponentUpdate 위해 불필요한 render 호출 방지
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        );
-        this.setState({
-          contents:_contents
-        });
+        // var _contents = this.state.contents.concat(
+        //   {id:this.max_content_id, title:_title, desc:_desc}
+        // );
+        // this.setState({
+        //   contents:_contents
+        // });
       }.bind(this)}></CreateContent>
     }
+    else if(this.state.mode ==='update'){
+      var _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc){
+          // 내용 복제 해서 반영, 원본 수정 X -> immutable
+          var _contents = Array.from(this.state.contents); 
+          // id 같은 원소를 수정
+          var i = 0;
+          while(i < _contents.length){
+            if(_contents[i].id === _id){
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i += 1;
+          }
+          this.setState({
+            contents:_contents,
+            mode:'read'
+          });
+      }.bind(this)}></UpdateContent>
+    }
+    return _article;
+  }
 
+  // React에서는 props나 state 값이 바뀌면 해당되는 컴포넌트의 render함수가 호출되도록 약속
+  // props나 state 값이 바뀌면 화면이 다시 그려짐
+  render() {  
+    console.log('App render');
+    
     // render 내의 this는 해당 컴포넌트 가르킴
     console.log('render', this);
 
@@ -126,18 +156,14 @@ class App extends Component {
 
           {/* CRU는 특정 페이지로 이동해서 오퍼레이션 실행
               Delete는 버튼 클릭할 때 작동하도록 구현 */}
-
         <Control 
           onChangeMode={function(_mode){
             this.setState({
               mode:_mode
             });
         }.bind(this)}
-        >
-
-        </Control>
-
-        {_article}
+        ></Control>
+        {this.getContent()}
 
         {/*
         <Subject title="React" sub="For UI"></Subject>
